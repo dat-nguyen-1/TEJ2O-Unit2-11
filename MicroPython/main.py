e34bd0d9173f13f16546dd950dac826ce46185be
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-03-31 13:08:20 -0400

    Implement random number comparison on Micro:bit
    This module compares two random numbers and displays them based on user interactions.

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,42 @@
 """
 Created by: Mr. Coxall
 Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+This module will compare 2 random numbers.
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+# initialize variables
+random_number1 = randint(0. 99)
+random_number2 = randint(0. 99)
+
+# initialize display
+display.clear()
+display.show(Image.HAPPY)
+
+# main loop
+while True:
+    # handle button A press
+    if button_a.was_pressed():
+        # display 1st number
+        display.clear()
+        display.scroll("#: " + str(random_number1))
+
+    # handle button B press
+    if button_b.was_pressed():
+        # display 2nd number
+        display.clear()
+        display.scroll("#: " + str(random_number2))
+
+    # handle shake
+    if accelerometer.was_gesture("shake"):
+        # display comparing 1st number and 2nd number
+        display.clear()
+        if random_number1 > random_number2:
+            display.scroll(str(random_number1) + " > " + str(random_number2))
+        else:
+            display.scroll(str(random_number1) + " < " + str(random_number2))
+
+        # reset display
+        display.clear()
+        display.show(Image.HAPPY)
